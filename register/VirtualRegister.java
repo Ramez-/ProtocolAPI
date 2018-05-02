ffe261cb857d34413686f99c0aa73feb449dd012
ramezelbaroudy <ramezemadaiesec@gmail.com>
2018-05-02 12:34:44 +0200

    add virtual register not found exception

--- register/VirtualRegister.java
+++ register/VirtualRegister.java
@@ -8,6 +8,10 @@ public class VirtualRegister extends BaseRegister {
 		super(registerID);
 
 	}
+	
+	// constructor that takes word like "power" and then it finds in the list of constants I did for the value of it
+	// if it is present then return it 
+	// otherwise return exception
 
 	@Override
 	public RegisterType openRegister() {
